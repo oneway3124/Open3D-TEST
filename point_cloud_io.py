b25cd2df91db011aba1cbddfd29a41fd251bef9c
wang.david.wei <wangwei_scu@qq.com>
2022-07-19 13:38:36 +0800

    first version

---
+++ point_cloud_io.py
@@ -0,0 +1,10 @@
+import open3d as o3d
+
+if __name__ == "__main__":
+    pcd_data = o3d.data.PCDPointCloud()
+    print(
+        f"Reading pointcloud from file: fragment.pcd stored at {pcd_data.path}")
+    pcd = o3d.io.read_point_cloud(pcd_data.path)
+    print(pcd)
+    print("Saving pointcloud to file: copy_of_fragment.pcd")
+    o3d.io.write_point_cloud("copy_of_fragment.pcd", pcd)
